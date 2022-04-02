45a5111b297ab3931c9e8f19b01353bce06d29fb
alexfrst <alex.forest1026@gmail.com>
2022-04-02 21:33:00 +0200

    feat: Flags for transfer_learning compatible model generation

--- model/model.py
+++ model/model.py
@@ -2,12 +2,13 @@ import torch.nn as nn
 from torchvision import models
 
 
-def load_inception(nb_classes):
+def load_inception(nb_classes, transfer_learning=False):
     inception_v3 = models.inception_v3(pretrained=True)
 
-    # on indique qu'il est inutile de calculer les gradients des paramètres du réseau
-    for param in inception_v3.parameters():
-        param.requires_grad = False
+    if transfer_learning:
+        # on indique qu'il est inutile de calculer les gradients des paramètres du réseau
+        for param in inception_v3.parameters():
+            param.requires_grad = False
 
     # Parameters of newly constructed modules have requires_grad=True by default
     # Handle the auxilary net
@@ -19,24 +20,29 @@ def load_inception(nb_classes):
 
     return inception_v3
 
-def load_mobilenet(nb_classes):
+
+def load_mobilenet(nb_classes, transfer_learning=False):
     mobile_net = models.mobilenet_v2(pretrained=True)
 
-    # on indique qu'il est inutile de calculer les gradients des paramètres du réseau
-    for param in mobile_net.parameters():
-        param.requires_grad = False
+    if transfer_learning:
+        # on indique qu'il est inutile de calculer les gradients des paramètres du réseau
+        for param in mobile_net.parameters():
+            param.requires_grad = False
 
     # on remplace la dernière couche fully connected à 1000 sorties (classes d'ImageNet) par une fully connected à 6 sorties (nos classes).
     # par défaut, les gradients des paramètres cette couche seront bien calculés
-    mobile_net.classifier[1] = nn.Linear(in_features=mobile_net.classifier[1].in_features, out_features=nb_classes, bias=True)
+    mobile_net.classifier[1] = nn.Linear(in_features=mobile_net.classifier[1].in_features, out_features=nb_classes,
+                                         bias=True)
 
     return mobile_net
 
+
 def get_params_tranfer_learning(model):
     params_to_update = []
-    for name,param in model.named_parameters():
+    for name, param in model.named_parameters():
         if param.requires_grad == True:
             params_to_update.append(param)
+
 
 if __name__ == '__main__':
     inception_v3 = load_inception(nb_classes=2)
