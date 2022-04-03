51cb8f17fa4c53b6ddbe1e97cd53ee536cb5f34c
alexfrst <alex.forest1026@gmail.com>
2022-04-03 08:52:38 +0200

    fix: Early Stopping problems

--- model/train.py
+++ model/train.py
@@ -28,7 +28,7 @@ def train_model(model, loader_train, data_val, optimizer, scheduler, criterion, 
                 pbar.set_postfix(**{"loss train": loss.item(), "loss val": loss_val, "Acc (val)": accuracy})
                 if accuracy > best_val_score:
                     best_val_score = accuracy
-                    torch.save(model, f'{training_name}.pt')
+                    torch.save(model.state_dict(), f'{training_name}.pt')
                 if tb_writer is not None:
                     tb_writer.add_scalar(f'{training_name}_Loss/train', loss.item(), epoch)
                     tb_writer.add_scalar(f'{training_name}_Loss/val', loss_val, epoch)
