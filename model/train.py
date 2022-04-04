c7d229e7540a0b26d2861ad2ca37d7c79fc03084
alexfrst <alex.forest1026@gmail.com>
2022-04-04 08:59:59 +0200

    feat: val at the end of each epoch

--- model/train.py
+++ model/train.py
@@ -6,7 +6,7 @@ import torch
 
 def train_model(model, loader_train, data_val, optimizer, scheduler, criterion, evaluate, n_epochs=10, device='cpu', tb_writer=None,training_name='train'):
     best_val_score = 0
-    best_model = copy.deepcopy(model.state_dict())
+    best_params = copy.deepcopy(model.state_dict())
     model.train(True)
     for epoch in range(n_epochs):  # à chaque epochs
         with tqdm(enumerate(loader_train), desc=f"Epoch {epoch}") as pbar:
@@ -22,13 +22,8 @@ def train_model(model, loader_train, data_val, optimizer, scheduler, criterion, 
                 else:
                     outputs = model(inputs)  # on calcule l'output
                     loss = criterion(outputs, labels)
-                model.train(False)
-                loss_val, accuracy = evaluate(model, data_val, device, criterion)
-                model.train(True)
+
                 pbar.set_postfix(**{"loss train": loss.item(), "loss val": loss_val, "Acc (val)": accuracy})
-                if accuracy > best_val_score:
-                    best_val_score = accuracy
-                    best_params = copy.deepcopy(model.state_dict())
                 if tb_writer is not None:
                     tb_writer.add_scalar(f'{training_name}_Loss/train', loss.item(), epoch)
                     tb_writer.add_scalar(f'{training_name}_Loss/val', loss_val, epoch)
@@ -38,8 +33,15 @@ def train_model(model, loader_train, data_val, optimizer, scheduler, criterion, 
                 loss.backward()  # on effectue la backprop pour calculer les gradients
                 optimizer.step()
         scheduler.step()
+        model.train(False)
+        loss_val, accuracy = evaluate(model, data_val, device, criterion)
+        model.train(True)
+        if accuracy > best_val_score:
+            best_val_score = accuracy
+            best_params = copy.deepcopy(model.state_dict())
+            torch.save(best_params, 'best_params.pt')
 
     if tb_writer is not None:
         tb_writer.flush()
-    model.load_state_dict(best_params)
+    model.load_state_dict(torch.load('best_params.pt'))
     return model, best_val_score
