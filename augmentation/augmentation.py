5515e435ed0c32d033a8e34e02a8f43c2a303db2
alexfrst <alex.forest1026@gmail.com>
2022-04-08 14:11:12 +0200

    feat: Best data augmentation pipeline for each network

--- augmentation/augmentation.py
+++ augmentation/augmentation.py
@@ -6,16 +6,24 @@ transforms_list = [(transforms.ColorJitter(brightness=0.1, hue=.1), 'ColorJitter
                    (transforms.GaussianBlur(kernel_size=(5, 9), sigma=(5, 10)), 'GaussianBlur-Medium'),
                    (transforms.RandomRotation(degrees=(0, 10)), 'Rotation-Small'),
                    (transforms.RandomRotation(degrees=(0, 180)), 'Rotation-Large'),
-                   (transforms.RandomResizedCrop(size=(299, 299),scale=(0.8,0.8)), 'Crop-Small'),
-                   (transforms.RandomResizedCrop(size=(299, 299),scale=(0.45,0.45)), 'Crop-Large'),
+                   (transforms.RandomResizedCrop(size=(299, 299), scale=(0.8, 0.8)), 'Crop-Small'),
+                   (transforms.RandomResizedCrop(size=(299, 299), scale=(0.45, 0.45)), 'Crop-Large'),
                    (transforms.RandomAdjustSharpness(sharpness_factor=1.2), 'Sharpness-Small'),
                    (transforms.RandomAdjustSharpness(sharpness_factor=2.0), 'Sharpness-Large'),
                    (transforms.RandomAutocontrast(p=0.2), 'Contrast-Small'),
                    (transforms.RandomAutocontrast(p=0.8), 'Contrast-Large'),
                    (transforms.AutoAugment(transforms.AutoAugmentPolicy.IMAGENET), 'AutoAugment-Imagenet')]
 
+resnext_best = transforms.Compose([
+    transforms.RandomRotation(degrees=(0, 180)),
+    transforms.RandomResizedCrop(size=(299, 299), scale=(0.8, 0.8)),
+    transforms.RandomAdjustSharpness(sharpness_factor=2.0),
+    transforms.RandomAutocontrast(p=0.8),
+    transforms.RandomAdjustSharpness(sharpness_factor=2.0),
+    transforms.GaussianBlur(kernel_size=(5, 9), sigma=(0.1, 5))])
 
-
-
-
-
+convnext_best = transforms.Compose([transforms.ColorJitter(brightness=0.1, hue=.1),
+                                    transforms.RandomResizedCrop(size=(299, 299), scale=(0.45, 0.45)),
+                                    transforms.RandomAutocontrast(p=0.8),
+                                    transforms.RandomAdjustSharpness(sharpness_factor=2.0),
+                                    transforms.GaussianBlur(kernel_size=(5, 9), sigma=(5, 10))])
