774eef7acdddb64c7f8306b0a666b130eac9a96f
alexfrst <alex.forest1026@gmail.com>
2022-04-10 06:32:32 +0200

    feat: Confusion matrix builder from test dataloader

--- model/evaluate.py
+++ model/evaluate.py
@@ -1,4 +1,5 @@
 import torch
+from sklearn.metrics import confusion_matrix
 def evaluate(model, dataset, device, criterion):
     avg_loss = 0.
     avg_accuracy = 0
@@ -15,3 +16,20 @@ def evaluate(model, dataset, device, criterion):
         avg_accuracy += batch_acc
 
     return avg_loss / len(dataset), float(avg_accuracy) / len(dataset)
+
+def get_confusion_matrix(model, dataset, device):
+    mat = None
+
+    classes = list(range(max([max(data[1]) for data in dataset])+1))
+
+    for data in dataset:
+        inputs, labels = data
+        inputs, labels = inputs.to(device), labels.to(device)
+        outputs = model(inputs)
+        _, preds = torch.max(outputs, 1)
+        print(torch.sum(preds == labels)/len(preds))
+        if mat is None:
+            mat = confusion_matrix(labels, preds,labels=classes)
+        else:
+            mat += confusion_matrix(labels, preds, labels=classes)
+    return mat
